875a32d1f5fbb2c6265d8efe89faae8e1eaeabaf
fc-anjos <felipe.anjos@usp.br>
2020-07-16 14:24:10 -0300

    Add capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 require "faker"
 require "spec_helper"
 require "rspec/rails"
+require 'factory_bot_rails'
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
