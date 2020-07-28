8aa7253e75cbefa54a0b0042e05ab0b8791410f8
fc-anjos <felipe.anjos@usp.br>
2020-07-28 19:32:14 -0300

    Add foreign key (users as authors) to Articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+2.times do
+  user = User.create(
+    {
+      name: Faker::Name.unique.name,
+    }
+  )
+  Article.create(author: user)
+end
+
