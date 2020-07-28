abc4d3c9dd400ba5992c0e14bce1571509192884
fc-anjos <felipe.anjos@usp.br>
2020-07-28 20:00:47 -0300

    Add categories to Articles

--- db/migrate/20200728214925_create_articles.rb
+++ db/migrate/20200728214925_create_articles.rb
@@ -2,6 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.integer :author_id, index: true, foreign_key: true
+      t.integer :category_id, index: true, foreign_key: true
       t.string :title
       t.string :text
       t.string :image
@@ -9,5 +10,6 @@ class CreateArticles < ActiveRecord::Migration[6.0]
     end
 
     add_foreign_key :articles, :users, column: :author_id
+    add_foreign_key :articles, :categories, column: :category_id
   end
 end
