423289cd9ebe8bf91c0b29311020e761ec21e707
fc-anjos <felipe.anjos@usp.br>
2020-07-30 19:01:49 -0300

    add titles and text for top post

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,8 @@ class Category < ApplicationRecord
   def self.categories_name
     Category.distinct.pluck(:name)
   end
+
+  def most_recent_article
+    articles.order('created_at desc').first
+  end
 end
