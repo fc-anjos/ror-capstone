77ee0beda0ef9cb4925d6efa4e45a592223ae9e9
fc-anjos <felipe.anjos@usp.br>
2020-08-03 09:45:16 -0300

    Add links throughout the application

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'categories/show'
   get 'articles/index'
   devise_for :users
-  resources :articles, only: %i[index]
+  resources :articles, only: %i[index show]
   resources :categories, only: %i[show]
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
