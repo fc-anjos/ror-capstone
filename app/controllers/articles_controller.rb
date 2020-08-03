cc81226e63b7e5ce4dae20632a7cefbdf7436cf6
fc-anjos <felipe.anjos@usp.br>
2020-08-03 18:57:52 -0300

    Add basic for for post creating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,25 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: %i[create new]
+
   def index; end
+
+  def new; end
+
+  def create
+    @article = Article.new(article_params)
+    @article.author_id = current_user
+    @article.save
+
+    if @article.save
+      redirect_to root_path, notice: 'Article was successfully created.'
+    else
+      render :index, alert: 'Article was not created.'
+    end
+  end
 
   private
 
-  def todo_params
+  def article_params
     params.require(:article).permit(:picture)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'categories/show'
-  get 'articles/index'
   devise_for :users
-  resources :articles, only: %i[index show]
+  resources :articles
   resources :categories, only: %i[show]
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
