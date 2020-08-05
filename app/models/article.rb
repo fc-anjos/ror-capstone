abb01bb163d872843e3514d9368febe7db448eef
fc-anjos <felipe.anjos@usp.br>
2020-08-05 14:01:29 -0300

    Add validations to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  validates_presence_of :categories, :image, :title
   mount_uploader :image, ImageUploader
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :categories
