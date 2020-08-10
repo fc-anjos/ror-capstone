4fe7b39223de62dd8a4f75d564ce6df2faf8da7c
fc-anjos <felipe.anjos@usp.br>
2020-08-10 19:00:28 -0300

    Add negative tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  context 'Fetch categories name' do
-    let!(:category) { create :category, :priority_1, :fashion }
-    let!(:user) { create :user }
-    let!(:article1) { build :article, author: user, categories: [category] }
-    let!(:article2) { build :article, author: user, categories: [category] }
-    let!(:vote) { create :vote, article: article1, user: user }
-    it 'appends this category to categories name' do
+  context 'Fetch most voted article' do
+    let(:user) { create :user }
+    let(:category) { create :category, :priority_1, :fashion }
+    let!(:article1) { create :article, author: user, categories: [category] }
+    let!(:article2) { create :article, author: user, categories: [category] }
+    let!(:article2) { create :article, author: user, categories: [category] }
+    let!(:vote) { create :vote, user: user, article: article1 }
+
+    it 'Grabs the most voted article' do
       expect(Article.most_voted).to eql(article1)
+    end
+
+    it 'And not the other ones' do
+      expect(Article.most_voted).not_to eql(article2)
     end
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,25 +5,34 @@ RSpec.describe Category, type: :model do
     let!(:category) { create :category, :priority_1, :fashion }
 
     it 'appends this category to categories name' do
-      expect(Category.categories_name).to eql([category.name])
+      expect(Category.categories_name).to include(category.name)
     end
   end
-  context 'Fetch most recent article' do
+  context 'Most recent article' do
     let(:user) { create :user }
     let(:category) { create :category, :priority_1, :fashion }
     let!(:article1) { create :article, author: user, categories: [category] }
     let!(:article2) { create :article, author: user, categories: [category] }
 
-    it 'Grabs the last created article' do
+    it 'Grabs the most recent article of this category' do
       expect(category.most_recent_article).to eql(article2)
+    end
+
+
+    it 'And not the former one' do
+      expect(category.most_recent_article).not_to eql(article1)
     end
   end
 
   context 'Fetch top category' do
     let!(:category1) { create :category, :priority_1, :fashion }
     let!(:category2) { create :category, :priority_2, :magazine }
-    it 'Grabs the last created article' do
+    it 'Grabs the top rated category' do
       expect(Category.top(1).first).to eql(category1)
+    end
+
+    it 'And not the other ones' do
+      expect(Category.top(1).first).not_to eql(category2)
     end
   end
 end
